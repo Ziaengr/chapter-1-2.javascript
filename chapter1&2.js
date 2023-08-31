96379ac367c2d55da23c8fbbe90a16844f9edbfd
Zia ur rehman <119785513+Ziaengr@users.noreply.github.com>
2023-08-31 10:38:38 -0700

    Add files via upload

---
+++ chapter1&2.js
@@ -0,0 +1,72 @@
+//Q :no:1
+//alert("Assalam alaikum");
+
+//Q:no:2
+//alert("Error!Please enter a valid password");
+
+//Q:no:3:
+//alert("Welcome to JS Land\nHappy Coding");
+
+//Q:no:4:
+//alert("Welcome toJS Land");
+//alert("Prevent this page from creating additional dialogs");
+
+//Q:no:5:
+//console.log("Hello...I can run JS through my web browser's console");
+
+//Chapter:2:
+
+//Q:no:1:
+//var username = 'zia'
+//alert(username)
+
+
+
+//Q:no:2:
+//var myname="Zia ur rehman"
+//alert(myname)
+
+//Q:no:3(a)
+//var titlemessage ='Hello World' 
+//Q:no:3(b)
+//alert(titlemessage)
+//Q:no:3(c)
+//alert("Hello world");
+
+
+//Q:no:4
+//var name='Zia ur rehman'
+//alert(name)
+//var year='20 Year Old'
+//alert(year)
+//var certificate= 'certified web and app development'
+//alert(certificate)
+
+
+//Q:no:5:
+//var email='kbkhanboy786@gmail.com'
+//alert(email)
+
+
+//Q:no:6
+//var greetmessage="PIZZA\nPIZZ\nPIZ\nPI\nP"
+//alert(greetmessage)
+
+//Q:no:7:
+//var sms="I am trying to learn for the Book A Smarter\nWay to learn Javascript";
+//alert(sms)
+
+
+//Q:no:8:
+//var greet="Yah! i can write HTML content through javascript"
+//alert(greet)
+
+//Q:no:9:
+//var greetingMessage ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+//alert(greetingMessage);
+
+
+
+
+
+
